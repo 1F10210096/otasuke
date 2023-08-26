3d6566d9fc88dde80beba5aa839660b92183fb73
1F10210096 <s1F102100962@iniad.org>
2023-08-27 04:28:13 +0900

    途中

--- server/usecase/msgUsecase.ts
+++ server/usecase/msgUsecase.ts
@@ -1,7 +1,26 @@
+import * as dotenv from 'dotenv';
+import { OpenAI } from 'openai';
+dotenv.config();
 
+
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY as string });
 
 export const msgUsecase = {
   sendMsg: async (msg: string): Promise<string> => {
-    return msg;
+    try {
+      const result = await openai.completions.create({
+        model: 'davinci-codex',
+        prompt: msg,
+        max_tokens: 50,
+      });
+
+      // "choices" プロパティから "text" プロパティを取得する
+      const answer: string = result.choices[0]?.text.trim();
+      return answer || 'No answer available.';
+    } catch (error) {
+      console.error('Error:', error);
+      return 'An error occurred.';
+    }
   },
 };
+
