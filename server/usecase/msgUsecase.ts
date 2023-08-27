67f15aec64c1632e959d9a5ff9bd54fd8debbbcb
1F10210096 <s1F102100962@iniad.org>
2023-08-27 15:32:33 +0900

    返事返ってくる

--- server/usecase/msgUsecase.ts
+++ server/usecase/msgUsecase.ts
@@ -2,20 +2,31 @@ import * as dotenv from 'dotenv';
 import { OpenAI } from 'openai';
 dotenv.config();
 
-
-const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY as string });
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 export const msgUsecase = {
   sendMsg: async (msg: string): Promise<string> => {
     try {
-      const result = await openai.completions.create({
-        model: 'davinci-codex',
-        prompt: msg,
+      const result = await openai.chat.completions.create({
+        model: 'gpt-4', // チャットモデルを指定
+        messages: [
+          {
+            role: 'system',
+            content: 'You are a helpful assistant.',
+          },
+          {
+            role: 'user',
+            content: msg,
+          },
+        ],
         max_tokens: 50,
       });
 
-      // "choices" プロパティから "text" プロパティを取得する
-      const answer: string = result.choices[0]?.text.trim();
+      const content = result.choices[0]?.message?.content;
+      const answer: string = content !== null ? content.trim() : '';
+      console.log(answer)
       return answer || 'No answer available.';
     } catch (error) {
       console.error('Error:', error);
@@ -23,4 +34,3 @@ export const msgUsecase = {
     }
   },
 };
-
