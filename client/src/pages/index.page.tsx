289f013900f204945936ae8cb05f0ac98becaed7
1F10210096 <s1F102100962@iniad.org>
2023-08-28 17:34:28 +0900

    記憶可能

--- client/src/pages/index.page.tsx
+++ client/src/pages/index.page.tsx
@@ -1,6 +1,7 @@
 import { SendOutlined } from '@ant-design/icons';
 import { AutoComplete, Button } from 'antd';
 import assert from 'assert';
+import dotenv from 'dotenv';
 import { useAtom } from 'jotai';
 import { ConversationChain } from 'langchain/chains';
 import { ChatOpenAI } from 'langchain/chat_models/openai';
@@ -12,35 +13,48 @@ import {
   MessagesPlaceholder,
   SystemMessagePromptTemplate,
 } from 'langchain/prompts';
-import { useCallback, useState } from 'react';
+import { useState } from 'react';
 import { Loading } from 'src/components/Loading/Loading';
 import { BasicHeader } from 'src/pages/@components/BasicHeader/BasicHeader';
-import { apiClient } from 'src/utils/apiClient';
 import { useSendMsg } from 'src/utils/sendMsg';
 import { userAtom } from '../atoms/user';
+dotenv.config();
 //a
+
 const Home = () => {
   const [user] = useAtom(userAtom);
   const [msg, setMsg] = useState('');
   const [myId, setmyId] = useState<string>('');
 
-  const createUserdata = useCallback(async () => {
-    console.log('a');
-    if (!user) return;
-    setmyId(user.id);
-    if (user === null) {
-      console.log('a');
-      await apiClient.create.$post();
-    } else {
-      if (user === null) {
-        console.log(user);
-      } else {
-        const userId = user.id;
-        const userroom = await apiClient.userCheck.$post({ body: { userId } });
-        // console.log(userroom);
-      }
-    }
-  }, [user]);
+  const run1 = async () => {
+    const model = new OpenAI({});
+    const memory = new BufferMemory();
+    const chain = new ConversationChain({ llm: model, memory });
+
+    const res1 = await chain.call({ input: 'こんにちは！ サガワです。' });
+    console.log({ res1 });
+
+    const res2 = await chain.call({ input: '私の名前はなんですか？' });
+    console.log({ res2 });
+  };
+
+  // const createUserdata = useCallback(async () => {
+  //   console.log('a');
+  //   if (!user) return;
+  //   setmyId(user.id);
+  //   if (user === null) {
+  //     console.log('a');
+  //     await apiClient.create.$post();
+  //   } else {
+  //     if (user === null) {
+  //       console.log(user);
+  //     } else {
+  //       const userId = user.id;
+  //       const userroom = await apiClient.userCheck.$post({ body: { userId } });
+  //       // console.log(userroom);
+  //     }
+  //   }
+  // }, [user]);
 
   const sendMsg = useSendMsg();
   //メッセージ送信
@@ -94,7 +108,7 @@ const Home = () => {
         icon={<SendOutlined />}
         style={{ position: 'fixed', top: 750, right: 300 }}
         type="primary"
-        onClick={() => sendMsgs()}
+        onClick={() => sendMsg(msg)}
       />
     </>
   );
