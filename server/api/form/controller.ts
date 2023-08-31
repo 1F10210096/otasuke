00ad1f796df0ed235d9ee7229953009efcdf3233
1F10210096 <s1F102100962@iniad.org>
2023-08-31 23:28:29 +0900

    form設定

--- server/api/form/controller.ts
+++ server/api/form/controller.ts
@@ -1,10 +1,9 @@
-import { formRepositry } from '$/repository/formRepositry';
 import { formUsecase } from '$/usecase/formUsecase';
 import { defineController } from './$relay';
 
 export default defineController(() => ({
-  post: async ({body}) => ({
+  post: async ({ body }) => ({
     status: 201,
-    body: await formUsecase.create(body.msg,body.name),
+    body: await formUsecase.create(body.msg, body.name),
   }),
 }));
